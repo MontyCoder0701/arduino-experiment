76187cd757ca0c9c0e66d2fec1d54461eb3a29c0
Soo <soojlee0106@naver.com>
2026-07-25 14:47:05 +0900

    Update hello_world.ino

--- hello_world/hello_world.ino
+++ hello_world/hello_world.ino
@@ -1,34 +1,36 @@
-// Minimal board test: wipe EEPROM, print Hello World on Serial.
-// Open Serial Monitor at 9600 baud.
-//
-// If you see Hello World, the board + USB serial are fine.
-// Built-in LED also blinks on pin 13 (Nano).
+// Button + OLED test.
+// Button: D2 to GND. OLED: SDA=A4, SCL=A5, VCC=5V, GND=GND.
+// Press = built-in LED and OLED light up.
 
-#include <EEPROM.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+
+const uint8_t BUTTON_PIN = 2;
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 void setup() {
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
   pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
 
-  // Wipe all EEPROM so old pet data is gone
-  for (int i = 0; i < EEPROM.length(); i++) {
-    EEPROM.update(i, 0xFF);
-  }
-
-  Serial.begin(9600);
-  while (!Serial && millis() < 3000) {
-    // wait briefly for Serial Monitor (USB boards); Nano continues anyway
-  }
-
-  Serial.println();
-  Serial.println("Hello World");
-  Serial.println("EEPROM cleared");
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  display.clearDisplay();
+  display.display();
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);
-  Serial.println("Hello World");
-  delay(1000);
+  // INPUT_PULLUP: pressed = LOW
+  bool pressed = digitalRead(BUTTON_PIN) == LOW;
+  digitalWrite(LED_BUILTIN, pressed ? HIGH : LOW);
 
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(1000);
+  if (pressed) {
+    display.fillScreen(WHITE);
+  } else {
+    display.clearDisplay();
+  }
+  display.display();
 }
